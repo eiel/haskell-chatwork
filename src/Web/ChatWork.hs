24f5260f61322e9a86daa73877870318fe7ba8a6
Tomohiko Himura <eiel.hal@gmail.com>
2016-04-30 21:46:41 +0900

    me endpointへリクエストできるように

--- src/Web/ChatWork.hs
+++ src/Web/ChatWork.hs
@@ -1,14 +1,31 @@
-{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Web.ChatWork ( baseURL ) where
 
+import Data.CaseInsensitive
+import Data.ByteString.Char8 as BS
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Types.Status ( statusCode )
+import Network.HTTP.Types.Header
 import System.Environment ( lookupEnv )
 
-baseURL :: String
-baseURL = "https://api.chatwork.com/v1"
+me token = get token meURL
 
-meURL :: String
 meURL = baseURL ++ "/me"
 
-getChatWorkTokenFromEnv :: IO Maybe String
-getChatWorkTokenFromEnv = lookupEnv "CHATWORK_TOKEN"
+get token path = do
+  manager <- newManager tlsManagerSettings
+
+  initRequest <- parseUrl meURL
+  let req = initRequest {
+        requestHeaders = [header token]
+      }
+  httpLbs req manager
+
+header token = ("X-ChatWorkToken", token)
+
+baseURL = "https://api.chatwork.com/v1"
+
+getChatWorkTokenFromEnv :: IO (Maybe ByteString)
+getChatWorkTokenFromEnv = fmap (fmap BS.pack) $ lookupEnv "CHATWORK_TOKEN"
